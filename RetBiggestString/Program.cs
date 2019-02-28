b5ea84627f61294a7c698e62e43fe3cf69e39c47
Alexander Anicete <alexander.anicete91@gmail.com>
2019-02-27 22:59:34 -0800

    Add project files.

---
+++ RetBiggestString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetBiggestString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = { "my", "little", "pony" , "asdfds" };
+            Console.WriteLine(GetMax(input)); 
+        }
+        public static string GetMax(string [] input)
+        {
+            string max = input[0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i].Length > input[i-1].Length)
+                {
+
+                    max = input[i];
+                }
+
+            }
+            return max;
+        }
+    }
+}
